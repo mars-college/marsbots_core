61fd4d3d254fd3e6c36bc816c26bd9d800825219
Jonathan Miller <jon@jmill.dev>
2022-03-10 16:08:40 -0800

    Add eden artgen change

--- marsbots_core/programs/eden_artgen.py
+++ marsbots_core/programs/eden_artgen.py
@@ -6,7 +6,14 @@ import discord
 from marsbots_core.resources.discord_utils import update_message
 
 
-async def generation_loop(client, token, message, ctx, refresh_interval: int):
+async def generation_loop(
+    client,
+    token,
+    og_message,
+    bot_message,
+    ctx,
+    refresh_interval: int,
+):
     task_id = str(uuid.uuid4())
     finished = False
     last_image = None
@@ -18,34 +25,36 @@ async def generation_loop(client, token, message, ctx, refresh_interval: int):
             filepath = f"{task_id}-testimage.png"
             output_img = result["output"]["creation"]
             output_img.save(filepath)
+            await update_progress(bot_message, 1)
             finished = True
             async with ctx.channel.typing():
                 local_file = discord.File(filepath, filename=filepath)
-                await message.reply("result", file=local_file)
+                await og_message.reply("result", file=local_file)
 
         elif status == "failed":
             finished = True
             async with ctx.channel.typing():
-                await message.reply("Something went wrong :(")
+                await og_message.reply("Something went wrong :(")
         else:
             progress = result["status"].get("progress")
             data = result["config"].get("data")
             latest_image = data.get("progress_image") if data else None
 
             if progress:
-                await update_progress(message, progress)
+                await update_progress(bot_message, progress)
 
             if latest_image and latest_image != last_image:
-                await update_image(message, latest_image)
+                await update_image(bot_message, latest_image)
                 last_image = latest_image
 
             time.sleep(refresh_interval)
 
 
 async def update_progress(message, progress):
-    if progress == '__none__':
+    if progress == "__none__":
         return
-    message_content = f"Generation is {int(progress*100)}% complete"
+    progress_num = min(int(progress * 100), 100)
+    message_content = f"Generation is {progress_num}% complete"
     await update_message(message, content=message_content)
 
 
