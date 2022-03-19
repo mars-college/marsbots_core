6ef762310d99ce3a5488a53accb14e598e56469a
Jonathan Miller <jon@jmill.dev>
2022-03-19 12:10:34 -0700

    Add disco_diffusion slash command with options

--- marsbots_core/util.py
+++ marsbots_core/util.py
@@ -3,3 +3,8 @@ import numpy as np
 
 def cosine_similarity(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+def hex_to_rgb_float(hex_str):
+    hex_str = hex_str.lstrip("#")
+    return tuple(int(hex_str[i : i + 2], 16) / 255.0 for i in (0, 2, 4))
