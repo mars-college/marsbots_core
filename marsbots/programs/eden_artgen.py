46a2d0081fd06f44d4a545cac609578db5ae4420
Jonathan Miller <jon@jmill.dev>
2022-07-14 21:10:36 -0400

    Changes to artgen

--- marsbots/programs/eden_artgen.py
+++ marsbots/programs/eden_artgen.py
@@ -12,22 +12,19 @@ from marsbots.resources.discord_utils import update_message
 async def generation_loop(
     gateway_url,
     minio_url,
+    interaction_message,
     config,
-    user_message,
-    bot_message,
-    ctx,
-    output_dir,
     refresh_interval: int,
 ):
     result = requests.post(gateway_url + "/request_creation", json=config)
 
-    if not await check_server_result_ok(result, bot_message):
+    if not await check_server_result_ok(result, interaction_message):
         return
 
     result = json.loads(result.content)
     task_id = result["task_id"]
     current_sha = None
-    await update_progress(bot_message, 0)
+    await update_progress(interaction_message, 0)
 
     while True:
         result = requests.post(
@@ -35,20 +32,20 @@ async def generation_loop(
             json={"task_id": task_id},
         )
 
-        if not await check_server_result_ok(result, bot_message):
+        if not await check_server_result_ok(result, interaction_message):
             return
 
         result = json.loads(result.content)
 
         if not result:
             message_suffix = "_Server error: task ID not found_"
-            message_content = appender(bot_message.content, message_suffix)
-            return await update_message(bot_message, content=message_content)
+            message_content = appender(interaction_message.content, message_suffix)
+            return await update_message(interaction_message, content=message_content)
 
         result = result[0]
         status = result["status"]
         progress = -1 if status == "pending" else result["status_code"]
-        await update_progress(bot_message, progress)
+        await update_progress(interaction_message, progress)
 
         if "intermediate_sha" in result:
             last_sha = result["intermediate_sha"][-1]
@@ -57,12 +54,12 @@ async def generation_loop(
                 sha_url = f"{minio_url}/{current_sha}"
                 filename = f"{current_sha}.png"
                 discord_file = await get_discord_file_from_url(sha_url, filename)
-                await update_image(bot_message, discord_file)
+                await update_image(interaction_message, discord_file)
 
         if status == "failed":
             message_suffix = "_Server error: Eden task failed_"
-            message_content = appender(bot_message.content, message_suffix)
-            return await update_message(bot_message, content=message_content)
+            message_content = appender(interaction_message.content, message_suffix)
+            return await update_message(interaction_message, content=message_content)
 
         if status not in ["pending", "running"]:
             break
@@ -103,10 +100,10 @@ async def get_discord_file_from_url(url, filename):
             return discord_file
 
 
-async def check_server_result_ok(result, bot_message):
+async def check_server_result_ok(result, interaction_message):
     if result.status_code != 200:
         error_message = result.content.decode("utf-8")
         message_suffix = f"_Server error: {error_message}_"
-        message_content = appender(bot_message.content, message_suffix)
-        await update_message(bot_message, content=message_content)
+        message_content = appender(interaction_message.content, message_suffix)
+        await update_message(interaction_message, content=message_content)
     return result.status_code == 200
